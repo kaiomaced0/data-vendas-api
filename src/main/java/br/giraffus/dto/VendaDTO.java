113a4a5ce9b9cb5697f41af6ccab3b45b7d17e7b
05562849259 <kaiomm2000@gmail.com>
2024-10-07 19:58:13 -0300

    ajustando nome parametro dto

--- src/main/java/br/giraffus/dto/VendaDTO.java
+++ src/main/java/br/giraffus/dto/VendaDTO.java
@@ -1,14 +1,13 @@
 package br.giraffus.dto;
 
-
 import java.time.LocalDate;
 import java.util.List;
 
 public record VendaDTO(
-        Long idCliente,
-        String observacao,
-        List<ItemProdutoDTO> itemProduto,
-        Integer quantidadeParcelas,
-        Integer intervaloParcelas,
-        LocalDate dataPrimeiraParcela) {
+                Long idCliente,
+                String observacao,
+                List<ItemProdutoDTO> listaItens,
+                Integer quantidadeParcelas,
+                Integer intervaloParcelas,
+                LocalDate dataPrimeiraParcela) {
 }
