8f5406ea895ce4380f5a75287af89b4b128fcd1d
05562849259 <kaiomm2000@gmail.com>
2024-10-07 19:59:43 -0300

    ajustando e add atributos novos s venda

--- src/main/java/br/giraffus/service/impl/VendaServiceImpl.java
+++ src/main/java/br/giraffus/service/impl/VendaServiceImpl.java
@@ -2,6 +2,7 @@ package br.giraffus.service.impl;
 
 import br.giraffus.dto.VendaDTO;
 import br.giraffus.dto.responseDTO.VendaResponseDTO;
+import br.giraffus.model.Cliente;
 import br.giraffus.model.EntityClass;
 import br.giraffus.model.ItemProduto;
 import br.giraffus.model.Usuario;
@@ -57,8 +58,9 @@ public class VendaServiceImpl implements VendaService {
             return Response.status(400).entity(e.getMessage()).build();
         }
     }
+
     @Override
-    public Response getAllSize(){
+    public Response getAllSize() {
 
         Usuario u = usuarioRepository.findByLogin(jsonWebToken.getSubject());
         try {
@@ -78,10 +80,9 @@ public class VendaServiceImpl implements VendaService {
         try {
             LOG.info("Requisição Venda.getId()");
             Venda venda = repository.findById(id);
-            if(venda.getAtivo() && venda.getEmpresa() == u.getEmpresa()) {
+            if (venda.getAtivo() && venda.getEmpresa() == u.getEmpresa()) {
                 return Response.ok(new VendaResponseDTO(venda)).build();
-            }
-            else{
+            } else {
                 throw new Exception();
             }
         } catch (Exception e) {
@@ -96,11 +97,18 @@ public class VendaServiceImpl implements VendaService {
         Usuario u = usuarioRepository.findByLogin(jsonWebToken.getSubject());
         try {
             LOG.info("Requisição Venda.insert()");
-            Venda venda =  new Venda();
-            venda.setCliente(clienteRepository.findById(vendaDTO.idCliente()));
+            Venda venda = new Venda();
+            if (vendaDTO.idCliente() != null) {
+
+                Cliente c = new Cliente();
+                c = clienteRepository.findById(vendaDTO.idCliente());
+                venda.setCliente(c);
+            }
             venda.setItemProdutos(new ArrayList<>());
             venda.setValorTotal(0.0);
-            vendaDTO.itemProduto().forEach(v -> {
+            venda.setPago(false);
+            venda.setFinalizada(false);
+            vendaDTO.listaItens().forEach(v -> {
                 ItemProduto i = new ItemProduto();
                 i.setProduto(produtoRepository.findById(v.idProduto()));
                 i.setQuantidade(v.quantidade());
@@ -109,7 +117,7 @@ public class VendaServiceImpl implements VendaService {
                 itemProdutoRepository.persist(i);
                 venda.setValorTotal(venda.getValorTotal() + i.getPreco());
                 venda.getItemProdutos().add(i);
-            } );
+            });
             venda.setEmpresa(u.getEmpresa());
             venda.setObservacao(vendaDTO.observacao());
             repository.persist(venda);
@@ -120,39 +128,40 @@ public class VendaServiceImpl implements VendaService {
         }
     }
 
-//    @Override
-//    @Transactional
-//    public Response update(VendaUpdateDTO vendaUpdateDTO) {
-//        try {
-//            LOG.info("Requisição Venda.update()");
-//            Venda venda = repository.findById(vendaUpdateDTO.id());
-//            if(venda.getAtivo()){
-//            venda.setCliente(clienteRepository.findById(vendaUpdateDTO.idCliente()));
-//            if(venda.getItemProdutos() == null){
-//                venda.setItemProdutos(new ArrayList<>());
-//            }
-//            vendaUpdateDTO.itemprodutos().forEach(v -> {
-//                ItemProduto i = new ItemProduto();
-//                i.setProduto(produtoRepository.findById(v.idProduto()));
-//                i.setQuantidade(v.quantidade());
-//                i.setPreco(v.quantidade() * i.getProduto().getValor());
-//                i.setAtivo(true);
-//                itemProdutoRepository.persist(i);
-//                venda.getItemProdutos().add(i);
-//            });
-//            venda.setObservacao(vendaUpdateDTO.observacao());
-//            venda.getItemProdutos().forEach(v -> venda.setValorTotal(venda.getValorTotal() + v.getPreco()));
-//
-//                return Response.ok(new VendaResponseDTO(venda)).build();
-//            }
-//            else{
-//                throw new Exception();
-//            }
-//        } catch (Exception e) {
-//            LOG.error("Erro ao rodar Requisição Venda.update()");
-//            return Response.status(400).entity(e.getMessage()).build();
-//        }
-//    }
+    // @Override
+    // @Transactional
+    // public Response update(VendaUpdateDTO vendaUpdateDTO) {
+    // try {
+    // LOG.info("Requisição Venda.update()");
+    // Venda venda = repository.findById(vendaUpdateDTO.id());
+    // if(venda.getAtivo()){
+    // venda.setCliente(clienteRepository.findById(vendaUpdateDTO.idCliente()));
+    // if(venda.getItemProdutos() == null){
+    // venda.setItemProdutos(new ArrayList<>());
+    // }
+    // vendaUpdateDTO.itemprodutos().forEach(v -> {
+    // ItemProduto i = new ItemProduto();
+    // i.setProduto(produtoRepository.findById(v.idProduto()));
+    // i.setQuantidade(v.quantidade());
+    // i.setPreco(v.quantidade() * i.getProduto().getValor());
+    // i.setAtivo(true);
+    // itemProdutoRepository.persist(i);
+    // venda.getItemProdutos().add(i);
+    // });
+    // venda.setObservacao(vendaUpdateDTO.observacao());
+    // venda.getItemProdutos().forEach(v ->
+    // venda.setValorTotal(venda.getValorTotal() + v.getPreco()));
+    //
+    // return Response.ok(new VendaResponseDTO(venda)).build();
+    // }
+    // else{
+    // throw new Exception();
+    // }
+    // } catch (Exception e) {
+    // LOG.error("Erro ao rodar Requisição Venda.update()");
+    // return Response.status(400).entity(e.getMessage()).build();
+    // }
+    // }
 
     @Override
     @Transactional
