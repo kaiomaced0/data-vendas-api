da9a6f57765532e10ff2289b9acb1f8887845e24
05562849259 <kaiomm2000@gmail.com>
2024-10-07 19:56:40 -0300

    adicionando os novos atributos no dto

--- src/main/java/br/giraffus/dto/ProdutoDTO.java
+++ src/main/java/br/giraffus/dto/ProdutoDTO.java
@@ -7,25 +7,32 @@ import java.util.List;
 
 public record ProdutoDTO(
         String nome,
+        String nomeLongo,
         String descricao,
         Integer estoque,
-        Double valor,
+        Double valorCompra,
         Double preco,
         Long idFornecedor,
         Integer estoqueMinimo,
         Long idMarca,
-        List<Long> idCategoria) {
+        List<Long> idCategoria,
+        String codigo,
+        String codigoBarras) {
 
     public static Produto criaProduto(ProdutoDTO produtoDTO) {
         Produto produto = new Produto();
         produto.setNome(produtoDTO.nome());
+        produto.setNomeLongo(produtoDTO.nomeLongo());
         produto.setDescricao(produtoDTO.descricao());
         produto.setEstoque(produtoDTO.estoque());
-        produto.setCusto(produtoDTO.valor());
+        produto.setCusto(produtoDTO.valorCompra());
         produto.setValor(produtoDTO.preco());
         produto.setEstoqueMinimo(produtoDTO.estoqueMinimo());
         produto.setCategorias(new HashSet<>());
-        // A implementação para setar a lista de categorias pode variar de acordo com sua implementação de backend.
+        produto.setCodigo(produtoDTO.codigo());
+        produto.setCodigoBarras(produtoDTO.codigoBarras());
+        // A implementação para setar a lista de categorias pode variar de acordo com
+        // sua implementação de backend.
         return produto;
     }
 }
