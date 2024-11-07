8902372bba343d9b10211c9175b522285f29adc5
05562849259 <kaiomm2000@gmail.com>
2024-11-07 19:15:50 -0300

    excluindo updatedto para usar apenas dto - produto

---
+++ src/main/java/br/giraffus/dto/responseDTO/ProdutoResponseAdminDTO.java
@@ -0,0 +1,32 @@
+package br.giraffus.dto.responseDTO;
+
+import br.giraffus.model.Produto;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public record ProdutoResponseDTO(
+        Long id,
+        String nome,
+        String descricao,
+        Integer estoque,
+        Double preco,
+        Long idFornecedor,
+        Long idMarca,
+        List<CategoriaResponseDTO> categorias,
+        Integer estoqueMinimo) {
+
+    public ProdutoResponseDTO(Produto produto) {
+        this(produto.getId(),
+                produto.getNome(),
+                produto.getDescricao(),
+                produto.getEstoque(),
+                produto.getValor(),
+                (produto.getFornecedor() != null) ? produto.getFornecedor().getId() : null,
+                (produto.getMarca() != null) ? produto.getMarca().getId() : null,
+                (produto.getCategorias() != null) ? produto.getCategorias().stream()
+                        .map(CategoriaResponseDTO::new)
+                        .collect(Collectors.toList()) : null,
+                produto.getEstoqueMinimo());
+    }
+}
